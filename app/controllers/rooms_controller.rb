9d2ebc1c71780b5cd93b1ef18766d318a0c311de
Andre Menezes <djdedemenezes@gmail.com>
2025-09-28 18:54:34 -0300

    feat: Add websocket broadcasting when updating a mediafile

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -3,6 +3,7 @@ class RoomsController < ApplicationController
   def show
     @room = Room.includes(:media_files).find_by(id: params[:id])
     @media_file = @room.media_files.find_by(active: true)
+    @media_file = MediaFile.new(title: "Próxima palestra em instantes") if @media_file.nil?
     # raise
   end
 end
