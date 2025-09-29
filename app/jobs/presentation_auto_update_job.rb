7df8a812a4176a08604f1f7ebad21a9c3902db0d
Andre Menezes <djdedemenezes@gmail.com>
2025-09-29 11:03:17 -0300

    refac job

--- app/jobs/presentation_auto_update_job.rb
+++ app/jobs/presentation_auto_update_job.rb
@@ -2,38 +2,45 @@ class PresentationAutoUpdateJob < ApplicationJob
   queue_as :default
 
   def perform(*args)
-    current_time = DateTime.new(2025,10,3, 11, 16)
+    @current_time = DateTime.new(2025,10,3, 11, 16)
+    # @current_time = Time.current
 
     # deactivate presentations
-    expired_presentations = Presentation
-                            .where(active: true)
-                            .where("end_time <= ?", current_time)
-
     expired_presentations.find_each do |presentation|
       presentation.update!(active: false)
-      Rails.logger.info("Deactivated expired: #{presentation.title} in #{presentation.room.name}")
-
-      # presentation.room.broadcast
+      Rails.logger.info("[RAILS::LOGGER::INFO] Deactivated expired: #{presentation.title} in #{presentation.room.name}")
     end
 
     # Activate presentations
-    current_presentations = Presentation
-                            .where(active: false)
-                            .where(
-                              "start_time <= ? AND end_time > ?",
-                              current_time, current_time
-                            )
-
     current_presentations.find_each do |presentation|
-      presentation
-        .room
-        .presentations
-        .where(active: true)
-        .update_all(active: false)
+      deactivate_room_active_presentations(presentation)
 
       presentation.update!(active: true)
-      Rails.logger.info "Activated current: #{presentation.title} in #{presentation.room.name}"
+      Rails.logger.info "[RAILS::LOGGER::INFO] Activated current: #{presentation.title} in #{presentation.room.name}"
     end
-    Rails.logger.info "Auto-update cycle completed"
+    Rails.logger.info "[RAILS::LOGGER::INFO] Auto-update cycle completed"
+  end
+
+  def expired_presentations
+    Presentation
+      .where(active: true)
+      .where("end_time <= ?", @current_time)
+  end
+
+  def current_presentations
+    Presentation
+      .where(active: false)
+      .where(
+        "start_time <= ? AND end_time > ?",
+        @current_time, @current_time
+      )
+  end
+
+  def deactivate_room_active_presentations(presentation)
+    presentation
+      .room
+      .presentations
+      .where(active: true)
+      .update_all(active: false)
   end
 end
