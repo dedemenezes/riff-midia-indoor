ed1aaf3e76ce0685611eebb6eec310c49cdf14f4
Andre Menezes <djdedemenezes@gmail.com>
2025-09-30 09:56:19 -0300

    config: hide user admin dashboard

--- app/avo/resources/user.rb
+++ app/avo/resources/user.rb
@@ -1,10 +1,11 @@
 class Avo::Resources::User < Avo::BaseResource
+  self.visible_on_sidebar = false
   # self.includes = []
   # self.attachments = []
   # self.search = {
   #   query: -> { query.ransack(id_eq: q, m: "or").result(distinct: false) }
   # }
-  
+
   def fields
     field :id, as: :id
     field :email, as: :text
