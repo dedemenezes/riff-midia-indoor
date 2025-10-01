2e24f810703a8782f9d18506cd647f6d0d9a6a30
Andre Menezes <djdedemenezes@gmail.com>
2025-10-01 18:04:48 -0300

    refac styling for index

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,15 +4,15 @@ module ApplicationHelper
       content_tag(
         :span,
         "Em andamento",
-        class: "fs-6 badge rounded-pill",
-        style: "background-color: rgb(220, 252, 231); color: rgb(22, 101, 52);"
+        class: "badge rounded-pill",
+        style: "font-size:18px;background-color: rgb(220, 252, 231); color: rgb(22, 101, 52);"
       )
     else
       content_tag(
         :span,
         "Próximo",
-        class: "fs-6 badge rounded-pill",
-        style: "color: rgb(30, 64, 175);background-color: rgb(219, 234, 254);"
+        class: "badge rounded-pill",
+        style: "font-size:18px;color: rgb(30, 64, 175);background-color: rgb(219, 234, 254);"
       )
     end
   end
