8f36f884bb69e9c07985e35e8a0ebc5814eeb406
Andre Menezes <djdedemenezes@gmail.com>
2025-10-04 09:15:38 -0300

    remove authenticatio from static presentations

--- app/controllers/static_presentations_controller.rb
+++ app/controllers/static_presentations_controller.rb
@@ -1,4 +1,6 @@
 class StaticPresentationsController < ApplicationController
+  skip_before_action :authenticate_user!
+
   def show
     @static_presentation = StaticPresentation.find_by id: params[:id]
   end
